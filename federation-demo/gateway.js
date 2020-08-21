519601bd54786203de923b253c2bd1ac4595198f
jessicaplatter <38014654+jessicaplatter@users.noreply.github.com>
2020-08-21 11:33:41 -0400

    Add files via upload

---
+++ federation-demo/gateway.js
@@ -0,0 +1,22 @@
+const { ApolloServer } = require("apollo-server");
+const { ApolloGateway } = require("@apollo/gateway");
+
+const gateway = new ApolloGateway();
+
+(async () => {
+  const server = new ApolloServer({
+    gateway,
+
+    // Apollo Graph Manager (previously known as Apollo Engine)
+    // When enabled and an `ENGINE_API_KEY` is set in the environment,
+    // provides metrics, schema management and trace reporting.
+    engine: false,
+
+    // Subscriptions are unsupported but planned for a future Gateway version.
+    subscriptions: false,
+  });
+
+  server.listen().then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`);
+  });
+})();
